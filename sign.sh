261d028eeb4c0d125225c01f772bfd19f3cec0f9
Jiří Techet <techet@gmail.com>
2019-10-12 12:26:29 +0200

    Fix the signing script on macOS Catalina
    For some reason the exported function in the script can't access
    the SIGN_CERTIFICATE variable any more - pass it as an argument of
    the function instead.

--- sign.sh
+++ sign.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sign() {
-	codesign -s "$SIGN_CERTIFICATE" --entitlements geany.entitlements --deep --force --strict=all --options runtime -vvv $1
+	codesign -s "$2" --entitlements geany.entitlements --deep --force --strict=all --options runtime -vvv "$1"
 }
 export -f sign
 
-sign "./Geany.app/Contents/MacOS/geany-bin"
-sign "./Geany.app/Contents/Resources/libexec/gnome-pty-helper"
-find ./Geany.app \( -name "*.dylib" -or -name "*.so" \) -exec sh -c 'sign "$0"' {} \;
-sign "./Geany.app"
+sign "./Geany.app/Contents/MacOS/geany-bin" "$SIGN_CERTIFICATE"
+sign "./Geany.app/Contents/Resources/libexec/gnome-pty-helper" "$SIGN_CERTIFICATE"
+find ./Geany.app \( -name "*.dylib" -or -name "*.so" \) -exec sh -c 'sign "$0" "$SIGN_CERTIFICATE"' {} \;
+sign "./Geany.app" "$SIGN_CERTIFICATE"
