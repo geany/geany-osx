e6ff68028b36cb06a9344e174d32891d7d3dfc89
Jiří Techet <techet@gmail.com>
2021-11-13 15:07:43 +0100

    Delete old Geany.app before creating bundle to be sure we have new files

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf ./Geany.app
 iconutil -c icns ./iconbuilder.iconset --output Geany.icns
 ~/.local/bin/gtk-mac-bundler geany.bundle
 cp -R Papirus Papirus-Dark ./Geany.app/Contents/Resources/share/icons
